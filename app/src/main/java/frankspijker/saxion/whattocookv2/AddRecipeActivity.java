d371d4f93260491300aaed8af602cacb3f3cee67
Frank Spijker <mail@frankspijker.com>
2019-04-08 15:54:44 +0200

    aaa

--- app/src/main/java/frankspijker/saxion/whattocookv2/AddRecipeActivity.java
+++ app/src/main/java/frankspijker/saxion/whattocookv2/AddRecipeActivity.java
@@ -3,6 +3,9 @@ package frankspijker.saxion.whattocookv2;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.DividerItemDecoration;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.widget.Button;
@@ -36,7 +39,11 @@ public class AddRecipeActivity extends AppCompatActivity {
 
         ingredientList = new ArrayList<>();
         ingredientList = IngredientProvider.getIngredientList();
-
+        layoutManager = new LinearLayoutManager(getApplicationContext());
+        addIngredients.setLayoutManager(layoutManager);
+        addIngredients.setItemAnimator(new DefaultItemAnimator());
+        addIngredients.addItemDecoration(new DividerItemDecoration(this, DividerItemDecoration.HORIZONTAL));
+        addIngredients.setAdapter(mAdapter);
     }
 
     public void saveRecipe(View v) {
--- app/src/main/java/frankspijker/saxion/whattocookv2/AddRecipeIngredientAdapter.java
+++ app/src/main/java/frankspijker/saxion/whattocookv2/AddRecipeIngredientAdapter.java
@@ -3,9 +3,11 @@ package frankspijker.saxion.whattocookv2;
 import android.content.Context;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import java.util.List;
@@ -14,29 +16,35 @@ public class AddRecipeIngredientAdapter extends RecyclerView.Adapter<AddRecipeIn
     private Context context;
     private List<Ingredient> ingredientList;
 
+    public class MyViewHolder extends RecyclerView.ViewHolder {
+        public TextView name;
+        public EditText description;
+        public Button addButton;
+
+        public MyViewHolder(View view) {
+            super(view);
+            name = view.findViewById(R.id.addRecipeName);
+            description = view.findViewById(R.id.addRecipeDescription);
+            addButton = view.findViewById(R.id.addButton);
+        }
+    }
+
     public AddRecipeIngredientAdapter(List<Ingredient> ingredientList, Context context) {
         this.ingredientList = ingredientList;
         this.context = context;
     }
 
-    public class MyViewHolder extends RecyclerView.ViewHolder {
-        public TextView name, description;
-        public Button addButton;
-
-        public MyViewHolder(View view) {
-            super(view);
-
-        }
-    }
-
-
     @Override
-    public AddRecipeIngredientAdapter.MyViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
-        return null;
+    public MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View itemView = LayoutInflater.from(parent.getContext()).inflate(R.layout.recyclerview_add_recipe, parent, false);
+        return new MyViewHolder(itemView);
     }
 
     @Override
-    public void onBindViewHolder(@NonNull AddRecipeIngredientAdapter.MyViewHolder myViewHolder, int i) {
+    public void onBindViewHolder(MyViewHolder holder, final int position) {
+        final Ingredient ingredient = ingredientList.get(position);
+        holder.name.setText(ingredient.getName());
+        holder.description.setText(ingredient.getAmount() + " " + ingredient.getAmountType());
 
     }
 
